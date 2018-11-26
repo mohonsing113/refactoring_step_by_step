ed608f62874ea3ac83f7d7dd575dd1f28cdf8859
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-26 15:15:43 +0800

    refactor public driver age to private

---
+++ src/test/java/com/tws/refactoring/test/PoliceTest.java
@@ -0,0 +1,17 @@
+package com.tws.refactoring.test;
+
+import com.tws.refactoring.Driver;
+import com.tws.refactoring.Police;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PoliceTest {
+
+    @Test
+    void should_check_driver_adult(){
+        assertTrue(new Police().checkDriver(new Driver(18)));
+        assertFalse(new Police().checkDriver(new Driver(17)));
+    }
+
+}
